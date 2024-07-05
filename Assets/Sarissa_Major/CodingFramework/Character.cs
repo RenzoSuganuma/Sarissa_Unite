cbbf0d65e5440c846b73a2d13d848296d8047755
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-07-06 01:14:05 +0900

    no message

---
+++ Assets/Sarissa_Major/CodingFramework/Character.cs
@@ -0,0 +1,20 @@
+using System;
+using Sarissa;
+using UnityEngine;
+
+public class Character : CodingFramework
+{
+    protected Single _healthPoint;
+
+    public virtual Single GetHealthPoint()
+    {
+        return _healthPoint;
+    }
+
+    public virtual void SetHealthPoint(Single value)
+    {
+        _healthPoint = value;
+    }
+    
+    
+}
---
+++ Assets/UnitTester.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+using Sarissa;
+
+public class UnitTester : CodingFramework
+{
+    private void Start()
+    {
+    }
+}
